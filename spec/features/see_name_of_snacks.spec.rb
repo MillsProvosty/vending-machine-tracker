db97feb5059e790b83a38a64a347d33eadd8bfe4
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-24 10:08:55 -0600

    It was failing because I hadn't added snacks to the previously written tests! It all passes, user story 2 is done

--- spec/features/see_name_of_snacks.spec.rb
+++ spec/features/see_name_of_snacks.spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'When a user visits a vending machine show page', type: :feature do
   scenario 'they see the names of all snacks associated with that vending machine, and price' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
-    snack1 = Snack.create(name: "Kit Kat", price: 1)
-    snack2 = Snack.create(name: "Reese's", price: 2)
-    snack3 = Snack.create(name: "Chips", price: 3)
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    snack1 = Snack.create!(name: "Kit Kat", price: 1)
+    snack2 = Snack.create!(name: "Reese's", price: 2)
+    snack3 = Snack.create!(name: "Chips", price: 3)
 
     visit machine_path(dons)
 save_and_open_page
@@ -17,12 +17,12 @@ save_and_open_page
   end
 
   it 'shows average price for each machine' do
-    
+
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
-    snack1 = dons.snacks.create(name: "Kit Kat", price: 1)
-    snack2 = dons.snacks.create(name: "Reese's", price: 2)
-    snack3 = dons.snacks.create(name: "Chips", price: 3)
+    snack1 = dons.snacks.create!(name: "Kit Kat", price: 1)
+    snack2 = dons.snacks.create!(name: "Reese's", price: 2)
+    snack3 = dons.snacks.create!(name: "Chips", price: 3)
 
 
     expect(page).to have_content("Average Price: $2")
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
   scenario 'they see the location of that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack1 = dons.snacks.create!(name: "Kit Kat", price: 1)
+    snack2 = dons.snacks.create!(name: "Reese's", price: 2)
+    snack3 = dons.snacks.create!(name: "Chips", price: 3)
 
     visit machine_path(dons)
 
